ffcffdb53b6728e949f5fa90e7c0428b740d5109
Hamda Hassan <hassanodowa7@gmail.com>
2025-08-12 13:00:09 +0300

    Update .gitignore and remove venv from tracking

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # <-- update URL
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # <-- delete URL
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),  # <-- update URL
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),  # <-- delete URL
 ]
