6b9f8892d05883c37e646744b5af2480e4094cb0
Hamda Hassan <hassanodowa7@gmail.com>
2025-08-17 23:55:49 +0300

    add

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,11 +2,15 @@
 from django import forms
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget  # <-- import TagWidget
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]
+        widgets = {
+            'tags': TagWidget(),  # <-- add the TagWidget here
+        }
 
 
 class CommentForm(forms.ModelForm):
